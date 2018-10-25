9fce0e937a8038c6a368d36417b75022b38e7e29
Ted Shaffer <ted@brightsign.biz>
2018-10-25 13:52:34 -0700

    index.d.ts - initial commit

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+/* tslint:disable:quotemark max-line-length */
+/* tslint:disable:quotemark max-line-length */
+/// <reference types="node" />
+export function bsBpfCConvertPresentation(buffer: Buffer): Function;
+export const getBpfxState: (state: any) => any;
